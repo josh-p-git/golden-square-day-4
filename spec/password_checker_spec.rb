52232e25c2def1859c325142c8c4fd486d133d81
alexHoggett <alexmh99@gmail.com>
2022-12-06 11:40:29 +0000

    added tests for password checker

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,5 +1,13 @@
-require 'PasswordChecker'
+require 'password_checker'
 
 describe "PasswordChecker class" do
-  
+  it "fails when password is invalid" do
+    passwordChecker = PasswordChecker.new()
+    expect { passwordChecker.check('test') }.to raise_error "Invalid password, must be 8+ characters."
+  end
+
+  it "returns true when password is long enough" do
+    passwordChecker = PasswordChecker.new()
+    expect(passwordChecker.check("alongpassword")).to eq true
+  end
 end
