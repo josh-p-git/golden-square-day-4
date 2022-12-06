4dff6383c98970ce5f2db641cf97162b409ae7a4
alexHoggett <alexmh99@gmail.com>
2022-12-06 15:05:06 +0000

    fixed reading_time bug

--- phase-2/lib/reading_time.rb
+++ phase-2/lib/reading_time.rb
@@ -23,7 +23,7 @@
 def reading_time(string)
   time_for_word = 60 / 200
   time_to_read = time_for_word * string.split.length / 60
-  return `#{time_to_read} minutes`
+  return "#{time_to_read} minutes"
 end
 
 string1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis magna eros, elementum dictum lorem ac, consequat mattis eros. Donec et scelerisque quam. Nunc dignissim maximus ex, interdum lacinia nisl luctus nec. Aliquam lorem nisl, scelerisque nec eros venenatis, mollis viverra felis. Sed sollicitudin et turpis a fermentum. Nulla pulvinar iaculis quam ut viverra. Aenean et arcu iaculis, vehicula urna a, sollicitudin odio. Vivamus tempor massa non orci commodo, sit amet tincidunt tellus posuere. Aenean risus urna, tristique et nunc et, blandit convallis lectus. Morbi quis enim velit. Etiam ut purus in nulla laoreet viverra. Morbi sit amet nisi vitae lorem vehicula feugiat. Donec id urna ac eros accumsan sodales at nec enim. Duis sagittis rutrum nulla, vitae lobortis lectus malesuada a. Vestibulum aliquet elit id massa fermentum tincidunt. In varius sed massa tempus ultrices. "
