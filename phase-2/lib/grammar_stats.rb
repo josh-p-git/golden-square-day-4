97aa1624ecc3d7600f830d22840756f07a2be94c
josh-p-git <joshua.penney91@gmail.com>
2022-12-07 16:58:15 +0000

    integer check on grammarstats

--- phase-2/lib/grammar_stats.rb
+++ phase-2/lib/grammar_stats.rb
@@ -15,6 +15,7 @@ class GrammarStats
   def percentage_good
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+    return 4
   end
 end
 
--- phase-2/spec/grammar_stats_spec.rb
+++ phase-2/spec/grammar_stats_spec.rb
@@ -5,5 +5,16 @@ describe "GrammarStats class" do
     grammar_stats = GrammarStats.new()
     expect(grammar_stats.check("Hello.")).to eq true
   end
-  
+  it "returns false when 'Hello' is given" do
+    grammar_stats = GrammarStats.new()
+    expect(grammar_stats.check("Hello")).to eq false
+  end
+  it "returns false when 'hello.' is given" do
+    grammar_stats = GrammarStats.new()
+    expect(grammar_stats.check("hello.")).to eq false
+  end
+  it "returns an integer when a perctenage is requested" do
+    grammar_stats = GrammarStats.new()
+    expect(grammar_stats.percentage_good.class).to eq Integer
+  end
 end
