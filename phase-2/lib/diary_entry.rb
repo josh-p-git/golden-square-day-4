bbeca41e7281d5ba042d9d7f4d9c12eaf893bbe9
alexHoggett <alexmh99@gmail.com>
2022-12-07 15:12:07 +0000

    added reading time to count_words class

--- phase-2/lib/diary_entry.rb
+++ phase-2/lib/diary_entry.rb
@@ -7,4 +7,20 @@ class DiaryEntry
     def title
         return @title
     end
+
+    def contents
+        return @contents
+    end
+
+    def count_words
+        @contents.split(" ").length
+    end
+
+    def reading_time(wpm)
+        return @contents.split(" ").length / wpm
+    end
 end
+
+diary = DiaryEntry.new("journal", "rsf sdf sdf")
+
+p diary.reading_time(100)
--- phase-2/spec/diary_entry_spec.rb
+++ phase-2/spec/diary_entry_spec.rb
@@ -5,8 +5,34 @@ require 'diary_entry'
         expect(diary_entry.instance_variable_get(:@title)).to eq "My journal"
         expect(diary_entry.instance_variable_get(:@contents)).to eq "secret"
     end
+
     it "It returns the title" do
         diary_entry = DiaryEntry.new("My journal", "secret")
         expect(diary_entry.title).to eq "My journal"
+    end
+
+    it "returns the contents" do
+        diary_entry = DiaryEntry.new("My journal", "secret")
+        expect(diary_entry.contents).to eq "secret"
+    end
+    
+    it "returns the number of words in the contents" do
+        diary_entry = DiaryEntry.new("My journal", "super secret words")
+        expect(diary_entry.count_words).to eq 3
+    end
+
+    it "returns the number of words in the contents" do
+        diary_entry = DiaryEntry.new("My journal", "super secret words")
+        expect(diary_entry.count_words).to eq 3
+    end
+
+    it "reading_time returns an integer value" do
+        diary_entry = DiaryEntry.new("My journal", "super secret words") 
+        expect(diary_entry.reading_time(2).class).to eq Integer
+    end
+
+    it "returns an estimate of the reading time given their reading speed" do
+        diary_entry = DiaryEntry.new("My journal", "super secret words")
+        expect(diary_entry.reading_time(100)).to eq 3/100
     end
 end
