39b8a192d4da162ec24c4c3198f33aeea7ed8b9d
alexHoggett <alexmh99@gmail.com>
2022-12-07 14:06:31 +0000

    finished most common letter

--- phase-2/lib/get_most_common_letter.rb
+++ phase-2/lib/get_most_common_letter.rb
@@ -1,10 +1,14 @@
 def get_most_common_letter(text)
+  text = text.tr(" !,.?", "")
   counter = Hash.new(0)
   text.chars.each do |char|
     counter[char] += 1
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  p counter.to_a
+  counter.to_a.sort_by { |k, v| -v }[0][0]
 end
+
+p get_most_common_letter("the roof, the roof, the roof is on fire!")
 
 # Intended output:
 # 
