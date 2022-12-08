45b27e3d9e3bfe698c1cb7172fe5b33bad19d319
josh-p-git <joshua.penney91@gmail.com>
2022-12-08 11:14:35 +0000

    finalised grammar stats

--- phase-2/lib/grammar_stats.rb
+++ phase-2/lib/grammar_stats.rb
@@ -1,6 +1,8 @@
 class GrammarStats
   def initialize
     # ...
+    @text_true = 0
+    @total = 0.to_f
   end
 
   def check(text) # text is a string
@@ -8,17 +10,16 @@ class GrammarStats
     # letter and ends with a sentence-ending punctuation mark.
 
     # puts text[-1](/[[:punct:]]/)
-    text[0] == text[0].upcase && /[[:punct:]]/.match?(text[-1])
-
+    check = text[0] == text[0].upcase && /[[:punct:]]/.match?(text[-1])
+      if check == true
+        @text_true += 1
+      end
+    @total += 1
   end
 
   def percentage_good
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
-    return 4
+    return (@text_true / @total * 100).to_i
   end
 end
-
-grammar_stats = GrammarStats.new()
-
-p grammar_stats.check("Hello!")
--- phase-2/spec/grammar_stats_spec.rb
+++ phase-2/spec/grammar_stats_spec.rb
@@ -3,18 +3,21 @@ require 'grammar_stats'
 describe "GrammarStats class" do
   it "returns a true when 'Hello.' is given" do
     grammar_stats = GrammarStats.new()
-    expect(grammar_stats.check("Hello.")).to eq true
+    expect(grammar_stats.check("Hello.")).to eq 1
   end
   it "returns false when 'Hello' is given" do
     grammar_stats = GrammarStats.new()
-    expect(grammar_stats.check("Hello")).to eq false
+    expect(grammar_stats.check("Hello")).to eq 1
   end
   it "returns false when 'hello.' is given" do
     grammar_stats = GrammarStats.new()
-    expect(grammar_stats.check("hello.")).to eq false
+    expect(grammar_stats.check("hello.")).to eq 1
   end
-  it "returns an integer when a perctenage is requested" do
+  it "returns 66% when two true and one false word entries are given" do
     grammar_stats = GrammarStats.new()
-    expect(grammar_stats.percentage_good.class).to eq Integer
-  end
+    grammar_stats.check("Hello.")
+    grammar_stats.check("false")
+    grammar_stats.check("Right.")
+    expect(grammar_stats.percentage_good).to eq 66
+end
 end
